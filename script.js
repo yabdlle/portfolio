cb19258a52b2fb5df4d75d2b069d6b306e10773c
ybdlle <masonzed101@gmail.com>
2024-08-24 21:24:56 -0500

    2

--- script.js
+++ script.js
@@ -85,3 +85,32 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 1000,
+  reset: false,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home-title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home-img", { delay: 400 });
+sr.reveal(".home-social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about-img", {});
+sr.reveal(".about-subtitle", { delay: 400 });
+sr.reveal(".about-text", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills-subtitle", {});
+sr.reveal(".skills-text", {});
+sr.reveal(".skills-data", { interval: 100 });
+// sr.reveal(".skills-img", { delay: 600 });
+
+/*SCROLL projects*/
+sr.reveal(".project-img", { interval: 200 });
+
+
